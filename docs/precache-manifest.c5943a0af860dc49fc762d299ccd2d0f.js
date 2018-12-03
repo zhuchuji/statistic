d9780c0accd4d3958c717c865d0ecd9eeb5ffe20
zhuchuji <1012536778@qq.com>
2018-12-03 22:44:52 +0800

    add production

---
+++ docs/precache-manifest.c5943a0af860dc49fc762d299ccd2d0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "cf821f004857604809a3",
+    "url": "./static/js/main.cf821f00.chunk.js"
+  },
+  {
+    "revision": "857a56960e60dd39aa5c",
+    "url": "./static/js/1.857a5696.chunk.js"
+  },
+  {
+    "revision": "cf821f004857604809a3",
+    "url": "./static/css/main.af680f5d.chunk.css"
+  },
+  {
+    "revision": "857a56960e60dd39aa5c",
+    "url": "./static/css/1.5619d150.chunk.css"
+  },
+  {
+    "revision": "790e9e545c0cf25ae68e8b79f6288a5c",
+    "url": "./index.html"
+  }
+];
